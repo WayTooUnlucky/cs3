33dcc1518dbf46925c0e094e7433fcace92b4bde
WayTooUnlucky <128545610+WayTooUnlucky@users.noreply.github.com>
2026-08-28 11:32:59 -0500

    syncing local docs
    uploading a mess of a program woooo

--- u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
+++ u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
@@ -12,21 +12,94 @@ import static java.lang.System.*;
 public class ExpressionSolver
 {
 	//add in instance variables
+   String ogExpression;
+   ArrayList<String> expression;
+   {
+      ogExpression = "";
+      expression = new ArrayList<String>();
+   }
 
 	public ExpressionSolver(String s)
-	{
+	{  setExpression(s);
 	}
 
 	public void setExpression(String s)
-	{
+	{  
+      ogExpression = s;
+      String[] expArray = s.split("");
+      for(String str : expArray)
+         expression.add(str);
 	}
 
 	public void solveExpression()
 	{
+      while(expression.contains("(")) {
+         String exp = "";
+         int i = expression.indexOf("(");
+         while(expression.contains(")")) {
+            if(expression.get(i).equals("[)(]"))
+               expression.remove(i);
+            else
+               exp += expression.remove(i);
+         }
+         ExpressionSolver nested = new ExpressionSolver(exp);
+         nested.solveExpression();
+         expression.add(i, nested.toString());
+      }
+      while(expression.contains("^")) {
+         int loc = expression.indexOf("^");
+         int left = Integer.parseInt(expression.get(loc - 1));
+         int right = Integer.parseInt(expression.get(loc + 1));
+         power(loc, left, right);
+      }
+      while(expression.contains("[*/]")) {
+         int loc = expression.indexOf("[*/]");
+         int left = Integer.parseInt(expression.get(loc - 1));
+         int right = Integer.parseInt(expression.get(loc + 1));
+         if(expression.get(loc).equals("*"))
+            multiply(loc, left, right);
+         else
+            divide(loc, left, right);
+      }
+      while(expression.contains("[+-]")) {
+         int loc = expression.indexOf("[+-]");
+         int left = Integer.parseInt(expression.get(loc - 1));
+         int right = Integer.parseInt(expression.get(loc + 1));
+         if(expression.get(loc).equals("+"))
+            add(loc, left, right);
+         else
+            subtract(loc, left, right);
+      }
 	}
+   
+   private void multiply(int loc, int left, int right) {
+      int product = left * right;
+      answerHandler(loc, product);
+   }
+   private void divide(int loc, int left, int right) {
+      int quotient = left / right;
+      answerHandler(loc, quotient);
+   }
+   private void add(int loc, int left, int right) {
+      int sum = left + right;
+      answerHandler(loc, sum);
+   }
+   private void subtract(int loc, int left, int right) {
+      int difference = left - right;
+      answerHandler(loc, difference);
+   }
+   private void power(int loc, int left, int right) {
+      int answer = (int)Math.pow(left, right);
+      answerHandler(loc, answer);
+   }
+   private void answerHandler(int loc, int answer) {
+      expression.set(loc - 1, "" + answer);
+      expression.remove(loc);
+      expression.remove(loc);
+   }
 
 	public String toString( )
 	{
-		return "";
+		return expression.get(0);
 	}
 }
--- u02a_arraylists/labs/04e - ExpressionSolver/Lab04e.java
+++ u02a_arraylists/labs/04e - ExpressionSolver/Lab04e.java
@@ -8,10 +8,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.Integer.*;
 import static java.lang.System.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Lab04e
 {
-	public static void main( String args[] )
+	public static void main( String args[] ) throws FileNotFoundException
 	{
+      Scanner scan = new Scanner(new File("lab04e.dat"));
+      while(scan.hasNextLine()) {
+         String expression = scan.nextLine();
+         ExpressionSolver solve = new ExpressionSolver(expression);
+         solve.solveExpression();
+         out.println(solve);
+      }
 	}
 }
