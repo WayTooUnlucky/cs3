20daa1df88aacda693c33c2cbe11598eb4d76812
WayTooUnlucky <128545610+WayTooUnlucky@users.noreply.github.com>
2026-09-01 11:38:23 -0500

    completed lab05a

--- u02c_iterators/labs/Lab 05A - Iterator/IteratorTest.java
+++ u02c_iterators/labs/Lab 05A - Iterator/IteratorTest.java
@@ -8,21 +8,32 @@ import static java.lang.System.*;
 class IteratorTest {
    public static void populateListFromString(ArrayList<String> list, 
                                                         String strList){   
-
+      list.clear();
+      addToListFromString(list, strList);
+      
    }
 
    public static void addToListFromString(ArrayList<String> list, 
                                                         String strList)   {   
-
+      Scanner scan = new Scanner(strList);
+      while(scan.hasNext())
+         list.add(scan.next());
    }
    
    /*** REMOVE ***/
    public static void remove(ArrayList<String> list, String toRemove) {
-                               
+      Iterator iter = list.iterator();
+      while(iter.hasNext()) {
+         if(iter.next().equals(toRemove))
+            iter.remove();
+      }
    }
 
    /*** REPLACE ***/
    public static void replace(ArrayList<String> list, String toRemove, String replaceWith) {
-
-   } 
+      ListIterator<String> iter = list.listIterator();
+      while(iter.hasNext())
+         if(iter.next().equals(toRemove))
+            iter.set(replaceWith);
+   }
 }
