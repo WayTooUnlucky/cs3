b06b4c63c17ddfccc9e8903b714d3a01907b9636
WayTooUnlucky <128545610+WayTooUnlucky@users.noreply.github.com>
2026-09-02 11:21:49 -0500

    lab 2 done

--- u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
@@ -14,5 +14,13 @@ public class Lab05b
 	public static void main ( String[] args )
 	{
 		//add test cases		
+      ListIteratorTest lsit = new ListIteratorTest("a b c a b c a +");
+      out.println(lsit);
+      
+      lsit.setTest("a b c d e f g h i j x x x x x 7");
+      out.println(lsit);
+      
+      lsit.setTest("1 2 3 4 5 6 a b c a b c b #");
+      out.println(lsit);
 	}
 }
--- u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
@@ -14,15 +14,29 @@ public class ListIteratorTest
 	private ArrayList<String> list;
 
 	public ListIteratorTest(String line) {
+      setTest(line);
 	}
 
 	public void setTest(String line) {
+      list = new ArrayList<>();
+      String[] words = line.split(" ");
+      String toRemove = words[words.length - 2];
+      String replaceWith = words[words.length - 1];
+      for(int i = 0; i < words.length; i++)
+         if(i < words.length - 2)
+            list.add(words[i]);
+      replace(toRemove, replaceWith);
 	}
 
 	public void replace(String toRemove, String replaceWith) {
+      ListIterator<String> lsit = list.listIterator();
+      while(lsit.hasNext())
+         if(lsit.next().equals(toRemove))
+            lsit.set(replaceWith);
+      
 	}
 
 	public String toString() {
-		return "";
+		return list.toString();
 	}
 }
